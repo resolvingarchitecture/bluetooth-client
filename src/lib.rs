5830743deff55313f1bd2d9db7571696ab8223d5
objectorange <objectorange@1m5.io>
2020-03-03 15:03:04 -0700

    comment out unused code

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,19 @@
 // use blurz::bluetooth_device::BluetoothDevice;
 // use blurz::BluetoothSession;
 
-/// Send Message using Bluetooth Radio
-pub fn send_message(msg: String) {
+// Send Message using Bluetooth Radio
+// pub fn send_message(msg: String) {
     // let path:
     // let session: BluetoothSession = BluetoothSession::create_session(&path).unwrap();
     // let adapter: BluetoothAdapter = BluetoothAdapter::init(&session).unwrap();
     // let device: BluetoothDevice = adapter.get_first_device().unwrap();
     // println!("{:?}", device);
-}
+// }
 
-/// Receive Message from Bluetooth Radio
-fn receive_message(msg: String) {
-
-}
+// Receive Message from Bluetooth Radio
+// fn receive_message(msg: String) {
+//
+// }
 
 #[cfg(test)]
 mod tests {
