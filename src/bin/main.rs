2cc1bde4ffdaa0be744128cd42f86da23d40a4c0
objectorange <brian@resolvingarchitecture.io>
2020-03-24 19:46:44 -0700

    add main; issues compiling blurz...libdbus will not build

---
+++ src/bin/main.rs
@@ -0,0 +1,14 @@
+extern crate log;
+extern crate simple_logger;
+
+use log::{trace,info};
+use bluetooth_client::BluetoothClient;
+
+fn main() {
+    simple_logger::init().unwrap();
+    trace!("Starting Bluetooth Client Daemon...");
+    let mut bt_client = BluetoothClient::new();
+    bt_client.init();
+
+    trace!("Bluetooth Client Daemon Stopped.");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,49 @@
+
 // use blurz::bluetooth_adapter::BluetoothAdapter;
 // use blurz::bluetooth_device::BluetoothDevice;
 // use blurz::BluetoothSession;
 
+use ra_common::models::{Network, Packet};
+
+use log::{trace,info,warn};
+
+pub struct BluetoothClient {
+
+}
+
+impl BluetoothClient {
+    pub fn new() -> Box<BluetoothClient> {
+        Box::new(BluetoothClient {
+
+        })
+    }
+    pub fn init(&mut self) {
+        info!("{}","Initializing TOR Client...")
+    }
+}
+
+impl Network for BluetoothClient {
+    fn handle(&mut self, packet: &mut Packet) {
+        unimplemented!()
+        // let path = packet.to_addr.as_str();
+        // let session = BluetoothSession::create_session(Option::Some(path)).unwrap();
+        // let adapter = BluetoothAdapter::init(&session).unwrap();
+        // let device = adapter.get_first_device().unwrap();
+        // info!("{:?}", device);
+        // match device.connect(5000) {
+        //     Ok(_) => (),
+        //     Err(e) => warn!("{:?}", e),
+        // }
+    }
+}
+
 // Send Message using Bluetooth Radio
 // pub fn send_message(msg: String) {
-    // let path:
-    // let session: BluetoothSession = BluetoothSession::create_session(&path).unwrap();
-    // let adapter: BluetoothAdapter = BluetoothAdapter::init(&session).unwrap();
-    // let device: BluetoothDevice = adapter.get_first_device().unwrap();
-    // println!("{:?}", device);
+//     let path:
+//     let session: BluetoothSession = BluetoothSession::create_session(&path).unwrap();
+//     let adapter: BluetoothAdapter = BluetoothAdapter::init(&session).unwrap();
+//     let device: BluetoothDevice = adapter.get_first_device().unwrap();
+//     println!("{:?}", device);
 // }
 
 // Receive Message from Bluetooth Radio
